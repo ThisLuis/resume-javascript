bc4e279a52d70ef205bdae2f66c616ea17598376
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-03-19 22:36:52 -0600

    promises.js ... chain

---
+++ 02-Fundamentos/promises.js
@@ -0,0 +1,82 @@
+const employers = [
+    {
+        id: 1,
+        name: 'Luis'
+    },
+    {
+        id: 2,
+        name: 'Karen'
+    },
+    {
+        id: 3,
+        name: 'Jorge'
+    },
+];
+
+const salaries = [
+    {
+        id: 1,
+        salary: 1000
+    },
+    {
+        id: 2,
+        salary: 1500
+    },
+];
+
+// Convert to promise...
+// const getEmployer = ( id, callback ) => {
+
+//     const employee = employers.find( e => e.id === id );
+
+//     if( employee ) {
+//         callback( null, employee );
+//     } else {
+//         callback(`El empleado con id ${ id } no existe`);
+//     }
+// }
+
+const getEmployer = ( id ) => {
+    return new Promise( ( resolve, reject ) => {
+        const employee = employers.find( (e) => e.id === id)?.name;
+
+        ( employee )
+            ? resolve( employee )
+            : reject( `No existe empleado con id ${id}` );
+    });
+
+}
+
+const getSalary = () => {
+    return new Promise( (resolve, reject ) => {
+        const salary = salaries.find( (s) => s.id === id)?.salary;
+
+        ( salary )
+            ? resolve( salary )
+            : reject( `El salario con id: ${ id } no existe ` );
+
+    });
+}
+
+const id = 3;
+// getEmployer(id)
+//     .then( employee => console.log( employee ))
+//     .catch( err => console.log(err) )
+
+// getSalary(id)
+//     .then( salary => console.log( salary ))
+//     .catch( err => console.log( err ))
+
+// Don't X
+getEmployer(id)
+    .then( employee => {
+
+        getSalary( id )
+            .then( salary => {
+                console.log('El empleado: ', employee, ' tiene un salario de: ', salary);
+            })
+            .catch( err => console.log(err))
+    })
+    .catch( err => console.log(err))
+
+// Chain promises ...
